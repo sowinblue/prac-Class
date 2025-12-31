b03379b967d7f534f07b8a17142d1d20dd866661
송우인 <sowintildeblue@gmail.com>
2025-12-31 10:40:12 +0900

    class 정의

---
+++ main.py
@@ -0,0 +1,11 @@
+class Person:
+    species = "Human"  # 클래스 변수, 모든 객체가 공유
+
+    def __init__(self, name):
+        self.name = name  # 인스턴스 속성
+
+p1 = Person("winiee")
+p2 = Person("Minam")
+
+print(p1.species)  # Human
+print(p2.species) 
